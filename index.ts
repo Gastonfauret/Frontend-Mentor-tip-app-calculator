86c431eca283d20d3973cb10a5f1c18eab457e40
Gaston Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-01-10 16:35:38 -0300

    Five Commit

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 const five = document.getElementsByClassName("five-percent");
-const bill = document.getElementsByClassName("bill-message").value;
-const people = document.getElementsByClassName("person-message").value;
+const bill = document.getElementsByClassName("bill-message");
+const people = document.getElementsByClassName("person-message");
 const tipMessage = document.getElementsByClassName("tip-price");
 const totalMessage = document.getElementsByClassName("total-price");
-tipMessage.innerHTML = "";
-totalMessage.innerHTML = "";
+tipMessage.innerTEXT = "";
+totalMessage.innerTEXT = "";
 
 five.addEventListener ("click", () => {
     function fivePercent () {
-        const tipValue.value = bill.value * 0.5
+        const tipValue.value = bill.value * 0.5;
         const totalTip.value = tipValue.value * people;
         tipMessage.innerHTML = `$${tipValue}`
         totalMessage.innerHTML = `$${totalTip}`
